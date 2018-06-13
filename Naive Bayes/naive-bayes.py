91cc9463ad06d1d63f03351ad2518c512026152d
Sadri Shehu <sadrishehu@gmail.com>
2018-06-13 15:30:18 +0200

    Algorithm Folders
    This commit is to apply changes in Algorithm Folders.
    Descriptions from each algorithm attached.

---
+++ Naive Bayes/naive-bayes.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Bayes’ Theorem is stated as:
+
+P(h|d) = (P(d|h) * P(h)) / P(d)
+
+Where
+
+P(h|d) is the probability of hypothesis h given the data d. This is called the posterior probability.
+P(d|h) is the probability of data d given that the hypothesis h was true.
+P(h) is the probability of hypothesis h being true (regardless of the data). This is called the prior probability of h.
+P(d) is the probability of the data (regardless of the hypothesis).
+You can see that we are interested in calculating the posterior probability of P(h|d) from the prior probability p(h) with P(D) and P(d|h).
+"""
---
+++ Random Forest/random-forest.py
@@ -0,0 +1,8 @@
+"""
+Source: From Wikipedia, the free encyclopedia
+Random forests or random decision forests are an ensemble learning method for classification, regression and other tasks, that operate by constructing a multitude of decision trees at training time and outputting the class that is the mode of the classes (classification) or mean prediction (regression) of the individual trees.[1][2] Random decision forests correct for decision trees' habit of overfitting to their training set.[3]:587–588
+
+The first algorithm for random decision forests was created by Tin Kam Ho[1] using the random subspace method,[2] which, in Ho's formulation, is a way to implement the "stochastic discrimination" approach to classification proposed by Eugene Kleinberg.[4][5][6]
+
+An extension of the algorithm was developed by Leo Breiman[7] and Adele Cutler,[8] and "Random Forests" is their trademark.[9] The extension combines Breiman's "bagging" idea and random selection of features, introduced first by Ho[1] and later independently by Amit and Geman[10] in order to construct a collection of decision trees with controlled variance.
+"""

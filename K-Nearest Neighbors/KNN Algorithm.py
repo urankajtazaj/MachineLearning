5b3b88480d95a389c2940e6c5b25bc7b6237153a
BlendArifaj <blendarifaj@hotmail.com>
2018-06-22 13:55:36 +0200

    Implementimi i Algoritmit KNN ne python

--- K-Nearest Neighbors/KNN Algorithm.py
+++ K-Nearest Neighbors/KNN Algorithm.py
@@ -77,19 +77,3 @@ class KNN:
             if self.testY[i] == self.predikimetY[i]:
                 saktesia +=1
         return round((saktesia/float(len(self.predikimetY)))*100,2)
-
-
-train = pd.read_csv("TrainDataTest.csv")
-test = pd.read_csv("TestDataTest.csv")
-
-trainY = train['Klasa']
-trainX = train.drop('Klasa',1)
-
-testY = test['Klasa']
-testX = test.drop('Klasa',1)
-
-objKNN = KNN(3)
-objKNN.trajnoAlgoritmin(trainX,trainY)
-objKNN.prediko(testX)
-objKNN.saktesiaAlgoritmit(testY)
-objKNN.predikimet()
